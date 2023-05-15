a4c738a2ecd3c5642ce93f8e82c26419e69404a2
piin64 <unghienlau@gmail.com>
2023-05-15 14:22:03 +0800

    Update mywebpage.js

--- mywebpage.js
+++ mywebpage.js
@@ -28,3 +28,17 @@ function toggleSocialMedia() {
     socialMedia.style.display = "none";
   }
 }
+
+window.onscroll = function() {
+    var scrollToTopButton = document.getElementById("scroll-to-top");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopButton.style.display = "block";
+    } else {
+        scrollToTopButton.style.display = "none";
+    }
+};
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
